bb20567312c4a59387d82a1504be0c6becf2c620
Guilherme Gomes <gguife747@gmail.com>
2023-05-17 11:21:31 -0300

    adding and connecting firebase

---
+++ src/FirebaseConfing.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBSGdUeV_wM_jnoE1Y6yIPxA2pn2vx5RKA",
+  authDomain: "wppclone-bccdf.firebaseapp.com",
+  projectId: "wppclone-bccdf",
+  storageBucket: "wppclone-bccdf.appspot.com",
+  messagingSenderId: "865067153780",
+  appId: "1:865067153780:web:365ff63e557405575b1421"
+};
